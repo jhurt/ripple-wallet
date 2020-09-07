ef6734ee7916c166b4c0f756f5bb19ede9f7c8fe
jhurt <jleehurt@gmail.com>
2020-09-07 14:32:40 -0700

    formatting

--- scripts/balance.js
+++ scripts/balance.js
@@ -1,98 +1,88 @@
-'use strict'
-const chalk = require('chalk')
-const minimist = require('minimist')
-const inquirer = require('inquirer')
-const RippleAPI = require('ripple-lib').RippleAPI
-const RippleAddressCodec = require('ripple-address-codec')
+'use strict';
+const chalk = require('chalk');
+const minimist = require('minimist');
+const inquirer = require('inquirer');
+const RippleAPI = require('ripple-lib').RippleAPI;
+const RippleAddressCodec = require('ripple-address-codec');
 
-console.log(chalk.green('-----------------------------------------------'))
-console.log(chalk.green('Ripple Wallet'), chalk.yellow('Balance Check'))
-console.log(chalk.green('-----------------------------------------------'), "\n")
+console.log(chalk.green('-----------------------------------------------'));
+console.log(chalk.green('Ripple Wallet'), chalk.yellow('Balance Check'));
+console.log(chalk.green('-----------------------------------------------'), "\n");
 
-const argv = minimist(process.argv.slice(3))
-const limitTransactions = argv.limit || 10
+const argv = minimist(process.argv.slice(3));
+const limitTransactions = argv.limit || 10;
 
 const getBalance = (address) => {
   const api = new RippleAPI({
     server: process.env.RIPPLE_API || 'wss://s2.ripple.com:443'
-  })
+  });
 
   api.connect().then(() => {
 
     // Show balances
-    console.log(chalk.yellow('Current Balance'))
+    console.log(chalk.yellow('Current Balance'));
     api.getBalances(address)
       .then(balances => {
-
         balances.filter(c => c.value > 0).map((currency) => {
           console.log(currency.value, currency.currency)
-        })
+        });
         console.log()
 
       }, fail)
       .then(() => {
-
         // Show recent transactions
         api.getTransactions(address, {
           limit: limitTransactions
         }).then(transactions => {
-
           if (transactions.length) {
-            console.log(chalk.yellow(`Last ${transactions.length} transactions`))
-            transactions.forEach(t => displayRecord(t, address))
+            console.log(chalk.yellow(`Last ${transactions.length} transactions`));
+            transactions.forEach(t => displayRecord(t, address));
             console.log()
           }
-
           process.exit(0)
         }, fail)
-
       })
-
   }).catch(fail)
-}
+};
 
 const displayRecord = (record, address) => {
-  const source = record.specification.source
-  const destination = record.specification.destination
+  const source = record.specification.source;
+  const destination = record.specification.destination;
 
   switch (record.type) {
     case 'payment':
-      let amount, currency, plusMinus, directionArrow, to
+      let amount, currency, plusMinus, directionArrow, to;
 
-      amount = +parseFloat(source.maxAmount.value).toFixed(5)
-      currency = source.maxAmount.currency
+      amount = +parseFloat(source.maxAmount.value).toFixed(5);
+      currency = source.maxAmount.currency;
 
       if (source.address === address) {
-        plusMinus = '-'
-        directionArrow =  '→'
-        to = destination.address
+        plusMinus = '-';
+        directionArrow = '→';
+        to = destination.address;
 
       } else {
-        plusMinus = '+'
-        directionArrow =  '←'
-        to = source.address
-
+        plusMinus = '+';
+        directionArrow = '←';
+        to = source.address;
       }
 
-      console.log(`${record.type}\t${plusMinus}${amount} ${currency} ${directionArrow} ${to}`)
-      break
+      console.log(`${record.type}\t${plusMinus}${amount} ${currency} ${directionArrow} ${to}`);
+      break;
     default:
-      console.log(`${record.type}`)
+      console.log(`${record.type}`);
       break
   }
-}
+};
 
 const fail = (message) => {
-  console.error(chalk.red(message), "\n")
+  console.error(chalk.red(message), "\n");
   process.exit(1)
-}
+};
 
 if (process.argv[3] && RippleAddressCodec.isValidAddress(process.argv[3])) {
-
   getBalance(process.argv[3])
-
 } else {
-
   const questions = [
     {
       type: 'input',
@@ -100,11 +90,10 @@ if (process.argv[3] && RippleAddressCodec.isValidAddress(process.argv[3])) {
       message: 'Enter wallet address:',
       validate: (value) => RippleAddressCodec.isValidAddress(value) ? true : 'Please enter a valid address'
     }
-  ]
+  ];
 
   inquirer.prompt(questions).then((answers) => {
-    console.log()
+    console.log();
     getBalance(answers.wallet)
   })
-
 }
--- scripts/pay.js
+++ scripts/pay.js
@@ -1,60 +1,54 @@
 'use strict'
-const chalk = require('chalk')
-const minimist = require('minimist')
-const inquirer = require('inquirer')
-const _get = require('lodash.get')
-const RippleAPI = require('ripple-lib').RippleAPI
-const RippleKeypairs = require('ripple-keypairs')
-const RippleAddressCodec = require('ripple-address-codec')
-const config = require('./config.json')
+const chalk = require('chalk');
+const minimist = require('minimist');
+const inquirer = require('inquirer');
+const _get = require('lodash.get');
+const RippleAPI = require('ripple-lib').RippleAPI;
+const RippleKeypairs = require('ripple-keypairs');
+const RippleAddressCodec = require('ripple-address-codec');
+const config = require('./config.json');
 
-console.log(chalk.green('-----------------------------------------------'))
-console.log(chalk.green('Ripple Wallet'), chalk.yellow('Make Payment'))
-console.log(chalk.green('-----------------------------------------------'), "\n")
+console.log(chalk.green('-----------------------------------------------'));
+console.log(chalk.green('Ripple Wallet'), chalk.yellow('Make Payment'));
+console.log(chalk.green('-----------------------------------------------'), "\n");
 
-const argv = minimist(process.argv.slice(2))
-const currency = argv.currency || config.currency
-const maxFee = argv['max-fee'] || config.maxFee
+const argv = minimist(process.argv.slice(2));
+const currency = argv.currency || config.currency;
+const maxFee = argv['max-fee'] || config.maxFee;
 
 const api = new RippleAPI({
   server: process.env.RIPPLE_API || 'wss://s1.ripple.com:443'
-})
+});
 
 const waitForBalancesUpdate = (sourceAddress, destinationAddress, origSourceBalance) => {
   Promise.all([
-    api.getBalances(sourceAddress, { currency: config.currency }),
-    api.getBalances(destinationAddress, { currency: config.currency })
+    api.getBalances(sourceAddress, {currency: config.currency}),
+    api.getBalances(destinationAddress, {currency: config.currency})
       .catch(handleActNotFound)
   ]).then((newBalances) => {
-
     if (_get(newBalances, '[0][0].value', 0) < origSourceBalance) {
-
-      console.log('New source balance:', chalk.green(_get(newBalances, '[0][0].value', 0), config.currency))
-
-      console.log('New destination balance:', chalk.green(_get(newBalances, '[1][0].value', 0), config.currency))
-
+      console.log('New source balance:',
+        chalk.green(_get(newBalances, '[0][0].value', 0), config.currency));
+      console.log('New destination balance:',
+        chalk.green(_get(newBalances, '[1][0].value', 0), config.currency));
       process.exit(0)
-
     } else {
-
-      setTimeout(() => waitForBalancesUpdate(sourceAddress, destinationAddress, origSourceBalance), 1000)
-
+      setTimeout(() => waitForBalancesUpdate(sourceAddress, destinationAddress, origSourceBalance), 1000);
     }
-
   })
-}
+};
 
 const handleActNotFound = (err) => {
   if (err.toString().indexOf('actNotFound') !== -1) {
-    return [{ currency: config.currency, value: '0' }]
+    return [{currency: config.currency, value: '0'}];
   }
   return Promise.reject(err);
-}
+};
 
 const fail = (message) => {
-  console.error(chalk.red(message), "\n")
-  process.exit(1)
-}
+  console.error(chalk.red(message), "\n");
+  process.exit(1);
+};
 
 const questions = [
   {
@@ -85,23 +79,23 @@ const questions = [
     message: 'Enter sender secret:',
     validate: (value) => {
       try {
-        RippleKeypairs.deriveKeypair(value)
-        return true
+        RippleKeypairs.deriveKeypair(value);
+        return true;
       } catch (e) {
-        return 'Invalid secret'
+        return 'Invalid secret';
       }
     }
   }
-]
+];
 
 inquirer.prompt(questions).then((answers) => {
-  const keypair = RippleKeypairs.deriveKeypair(answers.sourceSecret)
-  const sourceAddress = RippleKeypairs.deriveAddress(keypair.publicKey)
+  const keypair = RippleKeypairs.deriveKeypair(answers.sourceSecret);
+  const sourceAddress = RippleKeypairs.deriveAddress(keypair.publicKey);
 
   const instructions = {
     maxLedgerVersionOffset: 5,
     maxFee
-  }
+  };
 
   const payment = {
     source: {
@@ -119,29 +113,29 @@ inquirer.prompt(questions).then((answers) => {
         currency
       }
     }
-  }
+  };
 
   api.connect().then(() => {
     if (sourceAddress === answers.destinationAddress) {
       fail('Sender address not be the same as the destination address')
     }
-    console.log()
+    console.log();
 
     return Promise.all([
-      api.getBalances(sourceAddress, { currency: config.currency }),
-      api.getBalances(answers.destinationAddress, { currency: config.currency })
+      api.getBalances(sourceAddress, {currency: config.currency}),
+      api.getBalances(answers.destinationAddress, {currency: config.currency})
         .catch(handleActNotFound)
     ]).then((currentBalances) => {
-      const destinationBalance = +(_get(currentBalances, '[1][0].value', 0))
-      const sourceBalance = +(_get(currentBalances, '[0][0].value', 0))
-      const amount = +(answers.amount)
+      const destinationBalance = +(_get(currentBalances, '[1][0].value', 0));
+      const sourceBalance = +(_get(currentBalances, '[0][0].value', 0));
+      const amount = +(answers.amount);
 
-      console.log('Current destination balance:', chalk.green(destinationBalance, config.currency))
+      console.log('Current destination balance:', chalk.green(destinationBalance, config.currency));
       if (destinationBalance + amount < config.baseReserve) {
         fail(`Send at least ${config.baseReserve} ${config.currency} to create the destination address`)
-
       }
-      console.log('Current sender balance:', chalk.green(sourceBalance, config.currency))
+
+      console.log('Current sender balance:', chalk.green(sourceBalance, config.currency));
       if (sourceBalance - amount < config.baseReserve) {
         fail(`There should be at least ${config.baseReserve} ${config.currency} remaining at the sender address`)
       }
@@ -155,31 +149,19 @@ inquirer.prompt(questions).then((answers) => {
         }
       ]).then((confirm) => {
         if (!confirm.sure) {
-          process.exit()
+          process.exit();
         }
 
-        console.log("\nPreparing payment transaction...")
-
+        console.log("\nPreparing payment transaction...");
         return api.preparePayment(sourceAddress, payment, instructions).then(prepared => {
-
-          const { signedTransaction } = api.sign(prepared.txJSON, answers.sourceSecret)
-
-          console.log('Submitting payment...')
-
+          const {signedTransaction} = api.sign(prepared.txJSON, answers.sourceSecret);
+          console.log('Submitting payment...');
           return api.submit(signedTransaction).then(() => {
-
-            console.log('Waiting for balance to update (use Ctrl-C to abort)')
-
+            console.log('Waiting for balance to update (use Ctrl-C to abort)');
             waitForBalancesUpdate(sourceAddress, answers.destinationAddress, sourceBalance)
-
           }, fail)
-
         })
-
       })
-
     })
-
   }).catch(fail)
-
-})
+});
